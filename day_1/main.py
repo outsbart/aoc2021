c6b1c2fb0772d02b95039145c939e0d7e6051799
outsbart <outsbart@gmail.com>
2021-12-12 16:54:56 +0100

    day 1 part 2 solution

--- day_1/main.py
+++ day_1/main.py
@@ -8,9 +8,27 @@ def get_depth_measurements() -> Iterator[int]:
     """
     with open('input', 'r') as f:
         while line := f.readline():
-            yield line
+            yield int(line.strip())
 
 
-total = sum(d2 > d1 for d1, d2 in pairwise(get_depth_measurements()))
+def triplewise(iterable):
+    """
+    Return overlapping triplets from an iterable
+    eg: triplewise('ABCDEFG') -> ABC BCD CDE DEF EFG
+    """
+    for (a, _), (b, c) in pairwise(pairwise(iterable)):
+        yield a, b, c
 
-print(total)
+
+def count_consecutives_increases(iterable):
+    return sum(d2 > d1 for d1, d2 in pairwise(iterable))
+
+
+part_1 = count_consecutives_increases(get_depth_measurements())
+
+print(part_1)
+
+
+part_2 = count_consecutives_increases(map(sum, triplewise(get_depth_measurements())))
+
+print(part_2)
