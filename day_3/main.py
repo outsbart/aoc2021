b29d7bea5f45a66db452e82b7c627003fa7fb396
outsbart <outsbart@gmail.com>
2021-12-26 14:40:32 +0100

    day 3 part 2 solution

--- day_3/main.py
+++ day_3/main.py
@@ -1,3 +1,4 @@
+import operator
 from collections import defaultdict
 from typing import Iterator
 
@@ -29,7 +30,48 @@ def calc_power_rates(diagnostics) -> (gamma_rate, epsilon_rate):
     return gamma, gamma ^ int('1' * len(sums), 2)
 
 
+def most_common_value(numbers: list[str], position=0, func=operator.ge) -> str:
+    """
+    Determines the most common value in a given position for all given numbers
+    Returns '0' or '1'
+    """
+    return str(
+        int(
+            func(
+                sum(int(number[position]) for number in numbers),
+                len(numbers)/2
+            )
+        )
+    )
+
+
+def get_rating(func) -> int:
+    diagnostics = list(get_diagnostics())
+    position = 0
+
+    while len(diagnostics) > 1:
+        value = most_common_value(diagnostics, position=position, func=func)
+        diagnostics = [ d for d in diagnostics if d[position] == value ]
+        position += 1
+
+    return int(diagnostics[0], 2)
+
+
+def get_oxygen_generator_rating() -> int:
+    return get_rating(func=operator.ge)
+
+
+def get_co2_scrubber_rating() -> int:
+    return get_rating(func=operator.lt)
+
+
 g, e = calc_power_rates(get_diagnostics())
 
 part_1 = g * e
 print(part_1)
+
+
+oxygen = get_oxygen_generator_rating()
+co2 = get_co2_scrubber_rating()
+
+print(oxygen * co2)
